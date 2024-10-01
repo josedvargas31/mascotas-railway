25e683132c9760fc7721eb7829425b8308a09bf6
josedvargas31 <dajozavargas@gmail.com>
2024-09-30 21:47:01 -0500

    arreglos en el config

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
-export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || 'localhost';
-export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD = process.env.DB_PASSWORD || '';
-export const DB_NAME = process.env.DB_NAME || 'adopcion_mascotas';
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const PORT = 3000;
+export const DB_HOST = 'b25nfxtt2xstpgm0479p-mysql.services.clever-cloud.com';
+export const DB_USER = 'u00ujg0du6gsaps9';
+export const DB_PASSWORD = 'u00ujg0du6gsaps9';
+export const DB_NAME = 'b25nfxtt2xstpgm0479p';
+export const DB_PORT = 3306;
